ef5542d645b9e163817d0df05e1271e770fd4f27
gadelrab <gadelrab@mpi-inf.mpg.de>
2016-03-11 12:24:46 +0100

    adding type filtering

--- src/main/java/de/mpii/predicatelifting/YagoFactsFilter.java
+++ src/main/java/de/mpii/predicatelifting/YagoFactsFilter.java
@@ -1,6 +1,8 @@
 package de.mpii.predicatelifting;
 
 import com.google.common.collect.Multimap;
+import de.mpii.yagotools.YagoTaxonomy;
+import de.mpii.yagotools.YagoTypes;
 import de.mpii.yagotools.utils.YagoDataReader;
 import mpi.tools.basics3.Fact;
 import mpi.tools.javatools.util.FileUtils;
@@ -18,29 +20,43 @@ import java.util.Map;
 public class YagoFactsFilter {
 
 
+    private  YagoTypes yTypes;
+    private  YagoTaxonomy yTaxonomy;
     Multimap<String,String> predicateObject2Subjects;
 
-    public YagoFactsFilter(String reducedFactsPath){
+    public YagoFactsFilter(String reducedFactsPath, boolean loadTypes){
 
         predicateObject2Subjects= YagoDataReader.loadDataInMap(reducedFactsPath,null, YagoDataReader.MapType.PRED_OBJ_2_SUBJ);
+
+        if(loadTypes){
+            yTypes= YagoTypes.getInstance();
+            yTaxonomy= YagoTaxonomy.getInstance();
+
+        }
+
+
 
 
 
     }
 
-    public void predicateObjectsCount(String outputFile,int threshold){
+    public void predicateObjectsCount(String outputFile,int threshold,String type){
         try {
             BufferedWriter bw=FileUtils.getBufferedUTF8Writer(outputFile);
 
             for(String key : predicateObject2Subjects.keySet()){
                 Collection<String> values=predicateObject2Subjects.get(key);
                 int size=values.size();
+
                 if(size>=threshold) {
+
                     String[]items=key.split("\t");
                     values.forEach((v) -> {
                         try {
-                            bw.write(new Fact(v, items[0], items[1]).toTsvLine());
+                            if (type == null || yTaxonomy.getParents(yTypes.getType(v)).contains(type)) {
 
+                            bw.write(new Fact(v, items[0], items[1]).toTsvLine());
+                            }
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -67,14 +83,22 @@ public class YagoFactsFilter {
 //        args[1]="data/combined_predicates_filtered_count_100.tsv";
         if(args.length<3)
         {
-            System.out.println("Usage: filter_facts.sh <inFilePath> <outFilePath> <minimum support (integer)>");
+            System.out.println("Usage: filter_facts.sh <inFilePath> <outFilePath> <minimum support (integer)> <Type>");
             System.exit(0);
         }
 //        System.out.println(Arrays.toString(args));
+        boolean loadTypes=false;
+        String type=null;
+        if(args.length>3){
+            loadTypes=true;
+            type=args[3];
+        }
+
+
 
         int threshold=Integer.parseInt(args[2]);
-        YagoFactsFilter yf=new YagoFactsFilter(args[0]);
-        yf.predicateObjectsCount(args[1],threshold);
+        YagoFactsFilter yf=new YagoFactsFilter(args[0],loadTypes);
+        yf.predicateObjectsCount(args[1],threshold,type);
     }
 
 
