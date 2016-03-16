909fb4ca07cd7a84b2fd847b208f1a10f9866929
gadelrab <gadelrab@mpi-inf.mpg.de>
2016-03-16 10:29:10 +0100

    Group evaluation

--- src/main/java/de/mpii/frequentrulesminning/utils/HeadGroup.java
+++ src/main/java/de/mpii/frequentrulesminning/utils/HeadGroup.java
@@ -29,7 +29,7 @@ public class HeadGroup {
 
     @Override
     public boolean equals(Object obj) {
-        return headItems.equals(((HeadGroup)obj).headItems);
+        return Arrays.equals(headItems,((HeadGroup)obj).headItems);
     }
 
     public double getCoverage() {
