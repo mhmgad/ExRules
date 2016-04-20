989f053e00504a1523ecc4692ac0c23fe9e0fa1d
gadelrab <gadelrab@mpi-inf.mpg.de>
2016-04-20 20:08:46 +0200

    NEWLIFT experiment

--- run_experiment.sh
+++ run_experiment.sh
@@ -6,18 +6,18 @@ mkdir /GW/D5data-5/gadelrab/yago3/spmf/out2
 
 
 
-sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_f1_f2_LIFT.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -s NEW_LIFT -f1 -f2  -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_f1_f2_LIFT.prasp -stats
+sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_f1_f2_NEWLIFT.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -s NEW_LIFT -f1 -f2  -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_f1_f2_NEWLIFT.prasp -stats
 
 #no materialization
-sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_exceptions02_f1_f2_LIFT_ex_PNCONF.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -ex -exMinSup 0.02 -exRank PNCONF -s NEW_LIFT -f1 -f2  -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_exceptions02_f1_f2_LIFT_ex_PNCONF.prasp -stats
+sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_excep02_PNCONF_f1_f2_NEWLIFT.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -ex -exMinSup 0.02 -exRank PNCONF -s NEW_LIFT -f1 -f2  -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_excep02_PNCONF_f1_f2_NEWLIFT.prasp -stats
 
 
 #no weighted count
-sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_exceptions02_f1_f2_LIFT_ex_PNCONF_cPM05_noW.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -ex -exMinSup 0.02 -exRank PNCONF -s NEW_LIFT -f1 -f2 -pm -cPM 0.05 -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_exceptions02_f1_f2_LIFT_ex_PNCONF_cPM05_noW.prasp -dPM /GW/D5data-5/gadelrab/yago3/spmf/materialization05.tsv -stats
+sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_noWeight_f1_f2_NEWLIFT.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -ex -exMinSup 0.02 -exRank PNCONF -s NEW_LIFT -f1 -f2 -pm -cPM 0.05 -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_noWeight_f1_f2_NEWLIFT.prasp -dPM /GW/D5data-5/gadelrab/yago3/spmf/materialization05.tsv -stats
 
 
 #with PrASP and weihts
-sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_exceptions02_f1_f2_LIFT_ex_PNCONF_cPM05.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -ex -exMinSup 0.02 -exRank PNCONF -s NEW_LIFT  -w -f1 -f2 -pm -cPM 0.05 -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_body4_filtered2_exceptions02_f1_f2_LIFT_ex_PNCONF_cPM05.prasp -dPM /GW/D5data-5/gadelrab/yago3/spmf/materialization05.tsv -stats
+sh assemble/bin/mine_rules.sh -i /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_integer_transactions.tsv2 -o /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_weight_f1_f2_NEWLIFT.tsv -minConf 0.25    -de -m /GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine_mapping.tsv2 -ex -exMinSup 0.02 -exRank PNCONF -s NEW_LIFT  -w -f1 -f2 -pm -cPM 0.05 -oPrASP /GW/D5data-5/gadelrab/yago3/spmf/out2/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_weight_f1_f2_NEWLIFT.prasp -dPM /GW/D5data-5/gadelrab/yago3/spmf/materialization05.tsv -stats
 
 
 
