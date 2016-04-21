061251fe5fbf4a3d42d238594de085eb4a7d63eb
gadelrab <gadelrab@mpi-inf.mpg.de>
2016-04-21 22:44:19 +0200

    DLV to KB

--- src/main/java/de/mpii/frequentrulesminning/utils/DLV2Transactions.java
+++ src/main/java/de/mpii/frequentrulesminning/utils/DLV2Transactions.java
@@ -8,22 +8,27 @@ import java.io.IOException;
 public class DLV2Transactions {
 
 
-
+//TODO move all processing related to DLV here
     public static void main(String [] args) throws IOException {
+
+        // TODO use CLI
         RDF2IntegerTransactionsConverter cv=new RDF2IntegerTransactionsConverter();
 
 
         cv.parseDLVOutput(args[1]);
 
+        cv.loadPredicateMappingFromFile(args[2]);
+        cv.loadSubjectMappingFromFile(args[3]);
+
         switch (RDF2IntegerTransactionsConverter.EncodingType.valueOf(args[0])){
             case SPMF:
-                cv.exportTransactions(args[2]);
+                cv.exportTransactions(args[4]);
                 break;
             case RDF:
-                cv.exportToRDF(args[2]);
+                cv.exportToRDF(args[4]);
                 break;
             case PrASP:
-                cv.exportAsPrASP(args[2]);
+                cv.exportAsPrASP(args[4]);
                 break;
 
 
