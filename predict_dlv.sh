471d1100ca8d1c01b08b9ed09a6e37b85898a977
gadelrab <gadelrab@mpi-inf.mpg.de>
2016-04-24 20:52:27 +0200

    export Conflicts

--- predict_dlv.sh
+++ predict_dlv.sh
@@ -13,10 +13,14 @@ TOPK="$2"
 INPUT_DIR=`dirname $1`
 INPUT_FILE=`basename $1`
 
-OUTPUT_DIR=$INPUT_DIR/DLV
+OUTPUT_DIR=$INPUT_DIR/DLV2
+
+mkdir $OUTPUT_DIR
 
 
 KB_FILE=/GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine.dlv
+PREDICTS_MAPPING=/GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine.mapping_predicates
+SUBJECT_MAPPING=/GW/D5data-5/gadelrab/yago3/spmf/in/facts_to_mine.mapping_subjects
 
 INPUT_FILE_CONFLICT=$INPUT_DIR/$INPUT_FILE.conflict
 
@@ -33,3 +37,12 @@ head -$TOPK $INPUT_DIR/$INPUT_FILE > $INPUT_FILE_TOPK
 head -100 $INPUT_DIR/$INPUT_FILE_NEG > $INPUT_FILE_NEG_TOPK
 
 $DLV -nofacts $KB_FILE $INPUT_FILE_TOPK $INPUT_FILE_NEG_TOPK $INPUT_FILE_CONFLICT > $OUTPUT_FILE
+
+rm $INPUT_FILE_TOPK
+rm $INPUT_FILE_NEG_TOPK
+
+
+#run conflicts stats
+sh assemble/bin/dlv2kb.sh NONE $OUTPUT_FILE $PREDICTS_MAPPING $SUBJECT_MAPPING
+
+
---
+++ run_conflict_experiment.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+IN_DIRECT=/GW/D5data-5/gadelrab/yago3/spmf/out_LIFT2
+
+FILES=($IN_DIRECT/*.tsv.dlv)
+
+for FILE in $IN_DIRECT/*.tsv.dlv; do
+
+     FILE_SIZE=$(wc -l $FILE)
+     echo $FILE_SIZE
+
+
