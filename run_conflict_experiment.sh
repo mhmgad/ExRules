0204abd5e45769a31295a4708b8586327fbd488a
gadelrab <gadelrab@mpi-inf.mpg.de>
2016-04-25 15:08:28 +0200

    export Conflicts

--- run_conflict_experiment.sh
+++ run_conflict_experiment.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
 IN_DIRECT=/GW/D5data-5/gadelrab/yago3/spmf/out_LIFT2
-OUT_DIRCT=$IN_DIRECT/DLV_ALL3
+OUT_DIRCT=$IN_DIRECT/DLV_ALL4
 
 FILES=$(ls $IN_DIRECT/*.tsv.dlv)
 
 for i in `seq 1 10`;
     do
 
+    TOPK=$(perl -w -e "use POSIX; print ceil($i * 200 ), qq{\n}")
+    SUMMARY_FILE=OUT_DIRCT/summary.top$TOPK
     for FILE in $IN_DIRECT/*.tsv.dlv; do
-
+        INPUT_FILE_NAME='basename $2'
         FILE_SIZE=$(wc -l < $FILE)
 #        TOPK=$(perl -w -e "use POSIX; print ceil($i * 0.1 * $FILE_SIZE), qq{\n}")
-        TOPK=$(perl -w -e "use POSIX; print ceil($i * 1000 ), qq{\n}")
+
         echo "$i $TOPK $FILE Start"
         ./predict_dlv.sh $TOPK $FILE $OUT_DIRCT
         echo "$i $TOPK $FILE Done!"
+        head -2 $OUTPUT_DIR/$INPUT_FILE_NAME.predictions.top$TOPK.stats >$SUMMARY_FILE
+
+
                #  echo $(($i * 0.1 * $FILE_SIZE))
      done
 done
