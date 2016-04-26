fb4b9a56c28e062145a3f8b2611f1f923a35b171
gad <gadelrab@MPI-INF.MPG.DE>
2016-04-27 01:31:31 +0200

    IMDB experiment

--- run_IMDB_experiment.sh
+++ run_IMDB_experiment.sh
@@ -26,29 +26,31 @@ SORTING_TYPE=LIFT
 
 CPM_MINSUPP=0.0
 MIN_EXCEPT_SUPP=0.05
+MIN_EXCEPTION=0.001
+
 
 
 #normal horn rules
 
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp0001_conf25_100_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -s $SORTING_TYPE -f1 -f2  -oPrASP -oDLV -stats -oDLV_CONFLICT
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -s $SORTING_TYPE -f1 -f2  -oPrASP -oDLV -stats -oDLV_CONFLICT -minS $MIN_EXCEPTION
 
 #no materialization (support only)
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp0001_conf25_100_excep02_PNCONF_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE -f1 -f2  -oPrASP -oDLV -stats -oDLV_CONFLICT
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE -f1 -f2  -oPrASP -oDLV -stats -oDLV_CONFLICT -minS $MIN_EXCEPTION
 
 
 #no weighted count and order
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_noWeight_order_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV  -stats -oDLV_CONFLICT -PMo
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_noWeight_order_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV  -stats -oDLV_CONFLICT -PMo -minS $MIN_EXCEPTION
 
 
 #weights order
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_weight_order_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE  -w -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV -stats -oDLV_CONFLICT -PMo
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_weight_order_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE  -w -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV -stats -oDLV_CONFLICT -PMo -minS $MIN_EXCEPTION
 
 
 #no weighted and no order
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_noWeight_noOrder_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV  -stats -oDLV_CONFLICT
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_noWeight_noOrder_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV  -stats -oDLV_CONFLICT -minS $MIN_EXCEPTION
 
 
 #weights and no order
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp0001_conf25_100_excep02_PNCONF_cPM05_weight_noOrder_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE  -w -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV -stats -oDLV_CONFLICT
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_weight_noOrder_f1_f2_$SORTING_TYPE.tsv -minConf 0.25    -de -m $MAPPING_FILE -ex -exMinSup $MIN_EXCEPT_SUPP -exRank PNCONF -s $SORTING_TYPE  -w -f1 -f2 -pm -cPM $CPM_MINSUPP -oPrASP -oDLV -stats -oDLV_CONFLICT -minS $MIN_EXCEPTION
 
 
