5a4c907c530d57099f468c62024ddda370ba688c
gadelrab <gadelrab@mpi-inf.mpg.de>
2016-05-01 19:02:08 +0200

    experiment options

--- run_IMDB_experiment.sh
+++ run_IMDB_experiment.sh
@@ -7,16 +7,18 @@ RULE_MIN_SUPP=0.008
 RULE_MIN_CONF=0.6
 RULE_MAX_CONF=1
 PM_MINSUPP=0.0
+RM_FUNC=CONF
+
 
 FILTERS="-f1 -f2"
 
-OUTPUT_TYPES="-oPrASP -oDLV -stats -oDLV_CONFLICT"
+OUTPUT_TYPES="-oPrASP -oDLV -stats"# -oDLV_CONFLICT"
 
 
 FACTS_FILE=/GW/D5data-5/gadelrab/imdb/facts_to_mine_imdb.tsv
 
 
-OUT_DIRECTORY=/GW/D5data-5/gadelrab/imdb/out_$OUTPUT_SORTING_TYPE_
+OUT_DIRECTORY=/GW/D5data-5/gadelrab/imdb/out_RM-${RM_FUNC}_s-${OUTPUT_SORTING_TYPE}
 #/GW/D5data-5/gadelrab/yago3/spmf/out_LIFT_ALL
 
 mkdir -p $OUT_DIRECTORY
@@ -35,47 +37,50 @@ INPUT_TRANSACTIONS_FILE=$DATA_FILE.transactions
 
 PREDICATE_MAPPING_FILE=$DATA_FILE.mapping_predicates
 
+RUN_CONFIG_FILE=$OUT_DIRECTORY/experiment.prop
 
-echo "OUTPUT_SORTING_TYPE=$OUTPUT_SORTING_TYPE"
-echo "FACTS_FILE=$FACTS_FILE"
-echo "OUT_DIRECTORY=$OUT_DIRECTORY"
-echo "EXCEPTION_MIN_SUPP=$EXCEPTION_MIN_SUPP"
-echo "RULE_MIN_SUPP=$RULE_MIN_SUPP"
-echo "RULE_MIN_CONF=$RULE_MIN_CONF"
-echo "RULE_MAX_CONF=$RULE_MAX_CONF"
-echo "PM_MINSUPP=$PM_MINSUPP"
-echo "OUTPUT_TYPES=$OUTPUT_TYPES"
+echo "OUTPUT_SORTING_TYPE=$OUTPUT_SORTING_TYPE" > $RUN_CONFIG_FILE
+echo "FACTS_FILE=$FACTS_FILE" >> $RUN_CONFIG_FILE
+echo "OUT_DIRECTORY=$OUT_DIRECTORY" >> $RUN_CONFIG_FILE
+echo "EXCEPTION_MIN_SUPP=$EXCEPTION_MIN_SUPP" >> $RUN_CONFIG_FILE
+echo "RULE_MIN_SUPP=$RULE_MIN_SUPP" >> $RUN_CONFIG_FILE
+echo "RULE_MIN_CONF=$RULE_MIN_CONF" >> $RUN_CONFIG_FILE
+echo "RULE_MAX_CONF=$RULE_MAX_CONF" >> $RUN_CONFIG_FILE
+echo "PM_MINSUPP=$PM_MINSUPP" >> $RUN_CONFIG_FILE
+echo "RM_FUNC=$RM_FUNC" >> $RUN_CONFIG_FILE
+echo "OUTPUT_TYPES=$OUTPUT_TYPES" >> $RUN_CONFIG_FILE
+
 
 
 
 #normal horn rules
 echo "Mining Horn Rules"
 #sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_f1_f2_$OUTPUT_SORTING_TYPE.tsv -minConf $RULE_MIN_CONF    -de -m $PREDICATE_MAPPING_FILE -s $OUTPUT_SORTING_TYPE -f1 -f2  -oPrASP -oDLV -stats -oDLV_CONFLICT -minS $RULE_MIN_SUPP
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_f1_f2_$OUTPUT_SORTING_TYPE.tsv -minConf $RULE_MIN_CONF    -de -m $PREDICATE_MAPPING_FILE -s $OUTPUT_SORTING_TYPE $FILTERS  $OUTPUT_TYPES -minS $RULE_MIN_SUPP
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_Horn.tsv -minConf $RULE_MIN_CONF -maxConf $RULE_MAX_CONF    -de -m $PREDICATE_MAPPING_FILE -s $OUTPUT_SORTING_TYPE $FILTERS  $OUTPUT_TYPES -minS $RULE_MIN_SUPP
 
 
 #no materialization (support only)
 echo "Mining Revised Rules: Naive"
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_f1_f2_$OUTPUT_SORTING_TYPE.tsv -minConf $RULE_MIN_CONF    -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE $FILTERS  $OUTPUT_TYPES -minS $RULE_MIN_SUPP
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_Naive.tsv -minConf $RULE_MIN_CONF  -maxConf $RULE_MAX_CONF  -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE $FILTERS  $OUTPUT_TYPES -minS $RULE_MIN_SUPP
 
 
 #no weighted count and order
 echo "Mining Revised Rules: PMO"
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_noWeight_order_f1_f2_$OUTPUT_SORTING_TYPE.tsv -minConf $RULE_MIN_CONF    -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE $FILTERS -pm -cPM $PM_MINSUPP -oPrASP -oDLV  -stats -oDLV_CONFLICT -PMo -minS $RULE_MIN_SUPP
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_PMO.tsv -minConf $RULE_MIN_CONF   -maxConf $RULE_MAX_CONF -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE $FILTERS -pm -cPM $PM_MINSUPP $OUTPUT_TYPES -PMo -minS $RULE_MIN_SUPP
 
 
 #weights order
 echo "Mining Revised Rules: PMOW"
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_weight_order_f1_f2_$OUTPUT_SORTING_TYPE.tsv -minConf $RULE_MIN_CONF    -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE  -w $FILTERS -pm -cPM $PM_MINSUPP $OUTPUT_TYPES -PMo -minS $RULE_MIN_SUPP
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_PMOW.tsv -minConf $RULE_MIN_CONF  -maxConf $RULE_MAX_CONF  -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE  -w $FILTERS -pm -cPM $PM_MINSUPP $OUTPUT_TYPES -PMo -minS $RULE_MIN_SUPP
 
 
 #no weighted and no order
 echo "Mining Revised Rules: PM"
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_noWeight_noOrder_f1_f2_$OUTPUT_SORTING_TYPE.tsv -minConf $RULE_MIN_CONF    -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE $FILTERS -pm -cPM $PM_MINSUPP -oPrASP -oDLV  -stats -oDLV_CONFLICT -minS $RULE_MIN_SUPP
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_PM.tsv -minConf $RULE_MIN_CONF  -maxConf $RULE_MAX_CONF  -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE $FILTERS -pm -cPM $PM_MINSUPP $OUTPUT_TYPES -minS $RULE_MIN_SUPP
 
 
 #weights and no order
 echo "Mining Revised Rules: PMW"
-sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_spmf_supp001_conf25_100_excep02_PNCONF_cPM05_weight_noOrder_f1_f2_$OUTPUT_SORTING_TYPE.tsv -minConf $RULE_MIN_CONF    -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE  -w $FILTERS -pm -cPM $PM_MINSUPP $OUTPUT_TYPES -minS $RULE_MIN_SUPP
+sh assemble/bin/mine_rules.sh -i $INPUT_TRANSACTIONS_FILE -o $OUT_DIRECTORY/rules_PMW.tsv -minConf $RULE_MIN_CONF  -maxConf $RULE_MAX_CONF  -de -m $PREDICATE_MAPPING_FILE -ex -exMinSup $EXCEPTION_MIN_SUPP -exRank PNCONF -s $OUTPUT_SORTING_TYPE  -w $FILTERS -pm -cPM $PM_MINSUPP $OUTPUT_TYPES -minS $RULE_MIN_SUPP
 
 
